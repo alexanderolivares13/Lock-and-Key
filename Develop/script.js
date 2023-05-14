4a2aa247585aa62cbe2a2bdc13fd11ca3da2b474
alexanderolivares13 <124226984+alexanderolivares13@users.noreply.github.com>
2023-05-14 00:23:53 -0700

    set variables for character arrays, password criteria questions, and password length. Wrote a for loop that requires debugging

--- Develop/script.js
+++ Develop/script.js
@@ -6,53 +6,63 @@
 // Write the loop to be mindful of the selections True/False of the password requirements
 
 var generateBtn = document.querySelector("#generate");
+
 var alphaC = ["A", "B","C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 var lowerC = ["a", "b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  
 var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "`", "{", "|", "}", "~"];
+  
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var passwordLength = window.prompt("How many characters would you like to use? Between 8 - 128", 8);
-var indexLetter = Math.floor(Math.random() * alphaC.length);
+  
+var indexAletter = Math.floor(Math.random() * alphaC.length);
+  
+var indexLletter = Math.floor(Math.random() * lowerC.length);
+  
 var indexSpecial = Math.floor(Math.random() * special.length);
+  
 var indexNumber = Math.floor(Math.random() * numbers.length);
-var selector = Math.floor(Math.random() * 4);
-var randomChar = [alphaC[indexLetter], lowerC[indexLetter], special[indexSpecial], numbers[indexNumber]];
-var finalIndex = randomChar[selector];
-var generateRan = finalIndex;
+  
+// var randomChar = [alphaC[indexAletter], lowerC[indexLletter], special[indexSpecial], numbers[indexNumber]];
+  
+// var selector = Math.floor(Math.random() * randomChar.length);
+  
+var run = function generateRes() {
+   var finalIndex = randomChar[selector];
+   return finalIndex;
+}
+
 // Write password to the #password input
 function writePassword() {
-// console.log(finalIndex);
+    
+    var alphaChoice = window.confirm("Would you like to use Upper Case letters?");
+    console.log("Upper Case? " + alphaChoice);
+    
+    var lowerChoice = window.confirm("Would you like to use Lower Case letters?");
+    console.log("Lower Case? " + lowerChoice);
+    
+    var specialChoice = window.confirm("Would you like to use Special characters?");
+    console.log("Special Characters? " + specialChoice);
+  
+    var numberChoice = window.confirm("Would you like to use Numbers?");
+    console.log("Numbers? " + numberChoice);
+    
+    var passwordLength = window.prompt("How many characters would you like to use? Between 8 - 128", 8);
 
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
+    if (alphaChoice === true , lowerChoice === true , specialChoice === true , numberChoice === true) {
+      
+      for(var i = 0; i < passwordLength; i++){
+       run += "";
+       console.log("")
+      }
+    }
 
-  // passwordText.value = password;
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-function alphaChoice() {
-  var alphaChoice = window.confirm("Would you like to use Upper Case letters?");
-  console.log(alphaChoice);
-  return;
-}
-
-function lowerChoice() {
-  var lowerChoice = window.confirm("Would you like to use Lower Case letters?");
-  console.log(lowerChoice);
-  return;
-}
-
-function specialChoice() {
-  var specialChoice = window.confirm("Would you like to use Special characters?");
-  console.log(specialChoice);
-  return;
-}
-
-function numberChoice() {
-  var numberChoice = window.confirm("Would you like to use Numbers?");
-  console.log(numberChoice)
-  return;
-}
